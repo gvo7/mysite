0c6ea2ba5e26e10e87a975787ec4916b6ea04c6c
Gert Vossius <gert@vossius.me>
2016-11-02 00:30:21 +0100

    reorganize models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,11 @@
+from __future__ import unicode_literals
 from django.contrib import admin
-from .models import Integrator, Type, Vendor, Product, Device, Configuration_owner, Management
+from .models import Team, Service_status, Location, Integrator, Type, Vendor, Product, Device, Configuration_owner, Management
 
 # Register your models here.
+admin.site.register(Team)
+admin.site.register(Service_status)
+admin.site.register(Location)
 admin.site.register(Integrator)
 admin.site.register(Type)
 admin.site.register(Vendor)
--- inventory/models.py
+++ inventory/models.py
@@ -3,36 +3,53 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 
-class Configuration_owner(models.Model):
-    name = models.CharField(max_length=50)
+class Team(models.Model):
+    name = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=254)
+    business_phone = models.CharField(max_length=20)
+    standby_phone = models.CharField(max_length=20)
     teamleader = models.CharField(max_length=50)
     sdo = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
+
+#class Configuration_owner(models.Model):
+#    name = models.CharField(max_length=50)
+#    email = models.EmailField(max_length=254)
+#    teamleader = models.CharField(max_length=50)
+#    sdo = models.CharField(max_length=50)
+#
+#    def __str__(self):
+#        return self.name
+class Configuration_owner(models.Model):
+    name = models.ForeignKey(Team, to_field='name', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
+
 class Management(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 class Type(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Service_status(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 class Location(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     contact = models.CharField(max_length=100)
     contact_phone = models.CharField(max_length=20)
     contact_email = models.EmailField(max_length=254)
@@ -53,7 +70,7 @@ class Location(models.Model):
 
 
 class Integrator(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     email_support = models.EmailField(max_length=254)
     account_manager = models.CharField(max_length=100)
     email_account_manager = models.EmailField(max_length=254)
@@ -66,7 +83,7 @@ class Integrator(models.Model):
 
 
 class Vendor(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     email_support = models.EmailField(max_length=254)
     account_manager = models.CharField(max_length=100)
     email_account_manager = models.EmailField(max_length=254)
@@ -80,7 +97,7 @@ class Vendor(models.Model):
 
 class Product(models.Model):
     vendor = models.ForeignKey(Vendor)
-    product_name = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=50, unique=True)
     type =  models.ForeignKey(Type)
     eos_date = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
     eol_date = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
@@ -92,18 +109,22 @@ class Product(models.Model):
 
 
 class Device(models.Model):
-    assettag = models.CharField(max_length=20)
-    hostname = models.CharField(max_length=50)
+    assettag = models.CharField(max_length=20, unique=True)
+    hostname = models.CharField(max_length=50, unique=True)
     vendor = models.ForeignKey(Vendor)
     product = models.ForeignKey(Product)
-    serial_number = models.CharField(max_length=50)
-    configuration_owner = models.ForeignKey(Configuration_owner)
+    serial_number = models.CharField(max_length=50, unique=True)
+    #configuration_owner = models.ForeignKey(Configuration_owner)
+    operational_support_team = models.ForeignKey(Team, related_name='operational_support')
+    standby_support_team = models.ForeignKey(Team, related_name='standby_support')
     integrator = models.ForeignKey(Integrator)
     management_ipv4 = models.GenericIPAddressField(protocol='both', unpack_ipv4=False)
     management_ipv6 = models.GenericIPAddressField(protocol='both', unpack_ipv4=False, blank=True, null=True)
     management_interface = models.ForeignKey(Management)
     os_version = models.CharField(max_length=20)
     support_date = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    location = models.ForeignKey(Location)
+    service_status = models.ForeignKey(Service_status)
     last_backup = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
 
 
